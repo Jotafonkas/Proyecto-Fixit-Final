e15da98bffa90b530757f80145a72f001b3e8e1c
Juan Pozo Aravena <113263787+Jotafonkas@users.noreply.github.com>
2024-05-12 18:18:57 -0400

    Revert "Registro modificado, Creación Vista Ayuda, Modificación crear servicio"
    This reverts commit e4abd11b7f23a1a56ff66aeaea235d23e5c2a46e.

--- app/src/main/java/com/example/proyecto_fixit_final/Home.kt
+++ app/src/main/java/com/example/proyecto_fixit_final/Home.kt
@@ -2,8 +2,6 @@ package com.example.proyecto_fixit_final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.Intent
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
