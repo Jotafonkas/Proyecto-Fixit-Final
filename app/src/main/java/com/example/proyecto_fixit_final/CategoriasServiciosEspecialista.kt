e4abd11b7f23a1a56ff66aeaea235d23e5c2a46e
Matias Alfredo Jofré Rojas <notbito@gmail.com>
2024-05-12 17:37:55 -0400

    Registro modificado, Creación Vista Ayuda, Modificación crear servicio
    Se modifico el registro de especialista con sus redirecciones, para la vista ayuda tambien se hicieron las funcionalidades para dirigirse a enviar un correo o llamar, y se modifico la vista crear servicio.

---
+++ app/src/main/java/com/example/proyecto_fixit_final/CategoriasServiciosEspecialista.kt
@@ -0,0 +1,17 @@
+package com.example.proyecto_fixit_final
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class CategoriasServiciosEspecialista : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.categorias_especialista)
+
+    }
+}
---
+++ app/src/main/java/com/example/proyecto_fixit_final/CrearServicioEspecialista.kt
@@ -0,0 +1,13 @@
+package com.example.proyecto_fixit_final
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class CrearServicioEspecialista : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.crear_servicio_especialista)
+    }
+}
